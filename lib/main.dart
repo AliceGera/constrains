8a1465b3f8879f3fb467322f6d5f9a7220b2c65c
Alice Gerasimenko <alicegerasimenko@gmail.com>
2023-11-01 10:32:34 +0300

    [feature/SI-6283/] Constrains.

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Constrains`',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Constrains'),
     );
   }
 }
@@ -32,13 +32,66 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(),
+    final size = MediaQuery.of(context).size;
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          height: size.height,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ConstrainedBox(
+                constraints: const BoxConstraints(
+                  minWidth: 100,
+                  minHeight: 100,
+                  maxWidth: 200,
+                  maxHeight: 200,
+                ),
+                child: Container(
+                  height: 150,
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  width: 150,
+                  height: 150,
+                  child: Container(
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+              Container(
+                width: 200,
+                height: 120,
+                color: Colors.grey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      color: Colors.green,
+                      width: 30,
+                      height: 30,
+                    ),
+                    Container(
+                      color: Colors.blue,
+                      width: 60,
+                      height: 60,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
